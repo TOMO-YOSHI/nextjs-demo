f380036ee80aba1f08048933a327b8b45ffffadc
TOMO-YOSHI <lucky.paddy10@gmail.com>
2023-07-10 12:55:00 -0700

    add prefetch false to links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,31 +12,31 @@ export default function Home() {
       <br />
       <h2 className="text-center text-xl">Static</h2>
       <div className='text-center text-lg mt-2 underline'>
-        <Link href="/ssg/static">Access /ssg/static</Link>
+        <Link href="/ssg/static" prefetch={false}>Access /ssg/static</Link>
       </div>
       <br />
       <hr />
       <br />
       <h2 className="text-center text-xl">Revalidation</h2>
       <div className='text-center text-lg mt-2 underline'>
-        <Link href="/ssg/revalidate">Access /ssg/revalidate</Link>
+        <Link href="/ssg/revalidate" prefetch={false}>Access /ssg/revalidate</Link>
       </div>
       <div className='text-center text-lg mt-2 underline'>
-        <Link href="/ssg/ondemand-revalidate">Access /ssg/ondemand-revalidate</Link>
+        <Link href="/ssg/ondemand-revalidate" prefetch={false}>Access /ssg/ondemand-revalidate</Link>
       </div>
       <br />
       <hr />
       <br />
       <h2 className="text-center text-xl">Draft</h2>
       <div className='text-center text-lg mt-2 underline'>
-        <Link href="/ssg/draft">Access /ssg/draft</Link>
+        <Link href="/ssg/draft" prefetch={false}>Access /ssg/draft</Link>
       </div>
       <div className='text-center mt-6'>
-        <Link className='border-2 p-2 active:bg-yellow-400' href={draftAPIURL}>get draft cookie</Link>
+        <Link className='border-2 p-2 active:bg-yellow-400' href={draftAPIURL} prefetch={false}>get draft cookie</Link>
       </div>
       <br />
       <div className='text-center mt-2'>
-        <Link className='border-2 p-2 active:bg-yellow-400' href={clearCookieAPIURL}>clear draft cookie</Link>
+        <Link className='border-2 p-2 active:bg-yellow-400' href={clearCookieAPIURL} prefetch={false}>clear draft cookie</Link>
       </div>
     </main>
   )
