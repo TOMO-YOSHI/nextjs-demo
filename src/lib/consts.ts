937e099340e56cc47ea1f265d0285dd4882d330c
TOMO-YOSHI <lucky.paddy10@gmail.com>
2023-07-11 15:30:37 -0700

    update urls

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,15 +1,15 @@
 export const devDraftAPIURL = "http://localhost:3000/api/draft?secret=MY_SECRET_TOKEN&slug=/ssg/draft";
 
-export const prodDraftAPIURL = "https://nextjs-demo-seven-mocha.vercel.app/api/draft?secret=MY_SECRET_TOKEN&slug=/ssg/draft";
+export const prodDraftAPIURL = "https://nextjs-demo-tomo-yoshi.vercel.app/api/draft?secret=MY_SECRET_TOKEN&slug=/ssg/draft";
 
 export const devClearCookieAPIURL = "http://localhost:3000/api/clear-cookie?path=/ssg/draft";
 
-export const prodClearCookieAPIURL = "https://nextjs-demo-seven-mocha.vercel.app/api/clear-cookie?path=/ssg/draft";
+export const prodClearCookieAPIURL = "https://nextjs-demo-tomo-yoshi.vercel.app/api/clear-cookie?path=/ssg/draft";
 
 export const devRevalidateAPIURL = "http://localhost:3000/api/revalidate?secret=MY_SECRET_TOKEN&slug=/isr/ondemand-revalidate";
 
-export const prodRevalidateAPIURL = "https://nextjs-demo-seven-mocha.vercel.app/api/revalidate?secret=MY_SECRET_TOKEN&slug=/isr/ondemand-revalidate";
+export const prodRevalidateAPIURL = "https://nextjs-demo-tomo-yoshi.vercel.app/api/revalidate?secret=MY_SECRET_TOKEN&slug=/isr/ondemand-revalidate";
 
 export const devCookieCheckAPIURL = "http://localhost:3000/api/check-cookie";
 
-export const prodCookieCheckAPIURL = "https://nextjs-demo-seven-mocha.vercel.app/api/check-cookie";
+export const prodCookieCheckAPIURL = "https://nextjs-demo-tomo-yoshi.vercel.app/api/check-cookie";
